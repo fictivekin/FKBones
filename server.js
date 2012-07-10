35e158f21f086039188ef3876ea00a99f280c800
Evan Haas <evan@fictivekin.com>
2012-07-10 13:14:41 -0400

    set default port and interface

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var static = require('node-static');
 var fileServer = new static.Server('./webroot');
 
-var port = process.env.PORT;
-var ip = process.env.IP;
+var port = process.env.PORT || 8000;
+var ip = process.env.IP || "localhost";
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
